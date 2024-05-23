3376f27be8cd7970c9465cc72aea31af5faa1445
Pablo <pablofraustro@gmail.com>
2024-05-23 21:58:32 +0200

    deleted everything again

--- app/public/wp-content/plugins/simply-static/vendor/symfony/css-selector/Parser/Handler/NumberHandler.php
+++
@@ -1,5 +0,0 @@
-<br>
-<b>Fatal error</b>:  Uncaught Error: Interface &quot;Symfony\Component\CssSelector\Parser\Handler\HandlerInterface&quot; not found in /Users/pablofraustro/Local Sites/portfolio/app/public/wp-content/plugins/simply-static/vendor/symfony/css-selector/Parser/Handler/NumberHandler.php:29
-Stack trace:
-#0 {main}
-  thrown in <b>/Users/pablofraustro/Local Sites/portfolio/app/public/wp-content/plugins/simply-static/vendor/symfony/css-selector/Parser/Handler/NumberHandler.php</b> on line <b>29</b><br>
--- app/public/wp-content/themes/twentytwentytwo/functions.php
+++
@@ -1,5 +0,0 @@
-<br>
-<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_action() in /Users/pablofraustro/Local Sites/portfolio/app/public/wp-content/themes/twentytwentytwo/functions.php:33
-Stack trace:
-#0 {main}
-  thrown in <b>/Users/pablofraustro/Local Sites/portfolio/app/public/wp-content/themes/twentytwentytwo/functions.php</b> on line <b>33</b><br>
