a7ca9b809bf28b426db2e6be76204c1a8c05d1e8
Pablo <pablofraustro@gmail.com>
2024-06-28 12:47:17 +0200

    Removed themes_renamed files

--- app/public/wp-config.php
+++ app/public/wp-config.php
@@ -19,6 +19,9 @@
  * @package WordPress
  */
 
+define('WP_HOME', 'http://localhost:10003');
+define('WP_SITEURL', 'http://localhost:10003');
+
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', 'local' );
