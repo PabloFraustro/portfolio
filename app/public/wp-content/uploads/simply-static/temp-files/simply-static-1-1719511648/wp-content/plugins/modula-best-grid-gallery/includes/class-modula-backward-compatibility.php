412e3de302a9222c3fcec0711e1e7dbec0848368
Pablo <pablofraustro@gmail.com>
2024-06-28 10:03:45 +0200

    Generate static files
    Forgot to do it on the last commit

--- app/public/wp-content/uploads/simply-static/temp-files/simply-static-1-1719511648/wp-content/plugins/modula-best-grid-gallery/includes/class-modula-backward-compatibility.php
+++
@@ -1,6 +0,0 @@
-<br>
-<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_filter() in /Users/pablofraustro/Local Sites/portfolio/app/public/wp-content/plugins/modula-best-grid-gallery/includes/class-modula-backward-compatibility.php:12
-Stack trace:
-#0 /Users/pablofraustro/Local Sites/portfolio/app/public/wp-content/plugins/modula-best-grid-gallery/includes/class-modula-backward-compatibility.php(731): Modula_Backward_Compatibility-&gt;__construct()
-#1 {main}
-  thrown in <b>/Users/pablofraustro/Local Sites/portfolio/app/public/wp-content/plugins/modula-best-grid-gallery/includes/class-modula-backward-compatibility.php</b> on line <b>12</b><br>
