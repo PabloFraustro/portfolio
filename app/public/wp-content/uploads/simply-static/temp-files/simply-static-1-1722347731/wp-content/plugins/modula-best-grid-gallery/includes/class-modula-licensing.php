e766f8f704bf11decee6b5aa1c6f42d9f0c5f9b3
Pablo <pablofraustro@gmail.com>
2024-07-30 15:56:30 +0200

    added simple password

---
+++ app/public/wp-content/uploads/simply-static/temp-files/simply-static-1-1722347731/wp-content/plugins/modula-best-grid-gallery/includes/class-modula-licensing.php
@@ -0,0 +1,6 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_action() in /Users/pablofraustro/Local Sites/portfolio/app/public/wp-content/plugins/modula-best-grid-gallery/includes/class-modula-licensing.php:20
+Stack trace:
+#0 /Users/pablofraustro/Local Sites/portfolio/app/public/wp-content/plugins/modula-best-grid-gallery/includes/class-modula-licensing.php(107): Modula_Licensing-&gt;__construct()
+#1 {main}
+  thrown in <b>/Users/pablofraustro/Local Sites/portfolio/app/public/wp-content/plugins/modula-best-grid-gallery/includes/class-modula-licensing.php</b> on line <b>20</b><br>
---
+++ app/public/wp-content/uploads/simply-static/temp-files/simply-static-1-1722347731/wp-content/plugins/modula-best-grid-gallery/includes/public/meta/social_meta.php
@@ -0,0 +1,7 @@
+<!-- Modula Social Tags -->
+<meta property="og:type" content="<br />
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function esc_html() in /Users/pablofraustro/Local Sites/portfolio/app/public/wp-content/plugins/modula-best-grid-gallery/includes/public/meta/social_meta.php:3
+Stack trace:
+#0 {main}
+  thrown in <b>/Users/pablofraustro/Local Sites/portfolio/app/public/wp-content/plugins/modula-best-grid-gallery/includes/public/meta/social_meta.php</b> on line <b>3</b><br />
+">
