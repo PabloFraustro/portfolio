a26b668c98a06e429b375ba6f3223efa08a7763e
Pablo <pablofraustro@gmail.com>
2024-09-03 14:24:25 +0200

    Changed interaction projects
    width to 800px of content, some of the images are full now and clickable to a new page. Subtitles are centered.

--- app/public/wp-content/uploads/simply-static/temp-files/simply-static-1-1722510218/wp-content/plugins/modula-best-grid-gallery/includes/libraries/class-modula-review.php
+++
@@ -1,6 +0,0 @@
-<br>
-<b>Fatal error</b>:  Uncaught Error: Call to undefined function esc_html__() in /Users/pablofraustro/Local Sites/portfolio/app/public/wp-content/plugins/modula-best-grid-gallery/includes/libraries/class-modula-review.php:13
-Stack trace:
-#0 /Users/pablofraustro/Local Sites/portfolio/app/public/wp-content/plugins/modula-best-grid-gallery/includes/libraries/class-modula-review.php(156): Modula_Review-&gt;__construct()
-#1 {main}
-  thrown in <b>/Users/pablofraustro/Local Sites/portfolio/app/public/wp-content/plugins/modula-best-grid-gallery/includes/libraries/class-modula-review.php</b> on line <b>13</b><br>
