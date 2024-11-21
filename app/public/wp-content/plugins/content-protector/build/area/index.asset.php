2fb282a9f0999490aa1cf4b580b83ec439c5fe2c
Pablo <pablofraustro@gmail.com>
2024-11-21 13:11:51 +0100

    Deleted plugin
    Deleted plugin

--- app/public/wp-content/plugins/content-protector/build/area/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => '0d9236e8497c48c0f344');
--- app/public/wp-content/plugins/content-protector/inc/admin/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element'), 'version' => 'b5c561eed25e64dd6a40');
