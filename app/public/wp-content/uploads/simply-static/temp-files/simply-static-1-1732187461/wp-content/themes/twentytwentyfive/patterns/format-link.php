17ae8400b316e4b96022eb59c802630ab9b8014a
Pablo <pablofraustro@gmail.com>
2024-11-21 12:22:04 +0100

    Removed interaction
    Siplified galleries

---
+++ app/public/wp-content/uploads/simply-static/temp-files/simply-static-1-1732187461/wp-content/themes/twentytwentyfive/patterns/format-link.php
@@ -0,0 +1,11 @@
+<p><!-- wp:group {"className":"is-style-section-3","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40","right":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
+</p>
+<div class="wp-block-group is-style-section-3" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">
+	<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}}} -->
+	<p style="font-style:normal;font-weight:700"><br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function esc_html_e() in /Users/pablofraustro/Local Sites/portfolio/app/public/wp-content/themes/twentytwentyfive/patterns/format-link.php:17
+Stack trace:
+#0 {main}
+  thrown in <b>/Users/pablofraustro/Local Sites/portfolio/app/public/wp-content/themes/twentytwentyfive/patterns/format-link.php</b> on line <b>17</b><br>
+</p>
+</div>
