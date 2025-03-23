0e15e707f5dddb5af95a43451cafc94c2c76c8f2
Pablo <pablofraustro@gmail.com>
2025-03-23 12:10:00 +0100

    Added button manial ajnimaton

--- app/public/wp-content/uploads/simply-static/temp-files/simply-static-1-1742726653/wp-content/uploads/uag-plugin/assets/0/uag-js-183.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-9e2f4eb3' );
-});
-window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-0f8f2ab7' );
-});
- });
